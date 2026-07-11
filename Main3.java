3eba87aca4591244c30dac17b504cc6e8e08f4d1
YUAN.CHEN <yyh2205102568@gmail.com>
2026-07-11 16:42:10 +0900

    2-3 変数宣言 演習問題

---
+++ Main3.java
@@ -0,0 +1,13 @@
+/**
+ * HelloWorldプログラムのクラスです。
+ */
+public class Main3 {
+    public static void main(String[] args){
+        /*
+        *このプログラムは、画面にHello,World!を表示します。
+        */
+
+        //文字列を画面に出力します。
+        System.out.println("Hello,World!");
+    }
+}
