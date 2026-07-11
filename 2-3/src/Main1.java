cdbe830b1fc625dd1a2fb0ebecd9249558ab7d14
YUAN.CHEN <yyh2205102568@gmail.com>
2026-07-12 02:42:43 +0900

    2-3_変数宣言（演習問題） （YUAN CHEN)

---
+++ 2-3/src/Main1.java
@@ -0,0 +1,7 @@
+public class Main1 {
+    
+    public static void main(String[] args) {
+        System.out.println("今日の天気:晴れ");
+        System.out.println("気温:23.5℃");
+    }
+}
---
+++ 2-3/src/Main8.java
@@ -0,0 +1,16 @@
+/**
+ * 自己紹介を表示するプログラムです。
+ */
+public class Main8 {
+    public static void main(String[] args){
+        String name="山田太郎 ";
+        String birthPlace="東京都";
+        String favorteFood="カレーライス";
+
+        //自己紹介の内容を表示します。
+        System.out.println("====自己紹介====");
+        System.out.println("名前:"+name);
+        System.out.println("出身地:"+birthPlace);
+        System.out.println("好きな食べ物:"+favorteFood);
+    }
+}
