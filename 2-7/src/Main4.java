51d3f36f65ec9dcce766a4f6b3e0b8c47c7a6926
YUAN.CHEN <yyh2205102568@gmail.com>
2026-07-20 01:47:22 +0900

    Delete 2-7/src directory

--- 2-7/src/Main4.java
+++
@@ -1,9 +0,0 @@
-public class Main4 {
-    public static void main(String[] args){
-        double gram=315.8;
-        int price=138;
-        double total=gram/100*price;
-        int result=(int)total;
-        System.out.println("合計金額:"+result+"円です");
-    }
-}
