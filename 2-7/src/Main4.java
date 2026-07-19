6071430e3ad0f174d220b2bc1dea5c2078dc0fc5
YUAN.CHEN <yyh2205102568@gmail.com>
2026-07-20 01:47:57 +0900

    2-7_式と演算子（演習問題） YUAN CHEN

---
+++ 2-7/src/Main4.java
@@ -0,0 +1,9 @@
+public class Main4 {
+    public static void main(String[] args){
+        double gram=315.8;
+        int price=138;
+        double total=gram/100*price;
+        int result=(int)total;
+        System.out.println("合計金額:"+result+"円です");
+    }
+}
---
+++ 2-7/src/Main6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main6 {
+    public static void main(String[] agrs){
+        Scanner scanner =new Scanner(System.in);
+        System.out.print("現在の経験値を入力してください:");
+        String input=scanner.nextLine();
+        int exp=Integer.parseInt(input);
+        int result=(int)(exp*3.84);
+        System.out.println("ボーナス後の経験値:"+result);
+        scanner.close();
+
+    }
+    
+}
