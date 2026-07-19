52488cddfa0d4677207fe2bfa61cd5a365f4acfe
YUAN.CHEN <yyh2205102568@gmail.com>
2026-07-20 01:45:20 +0900

    Delete Main2.java

--- Main2.java
+++
@@ -1,8 +0,0 @@
-public class Main2 {
-    public static void main(String[] args){
-        int testScore=85;
-        int studentNumber=15;
-        boolean isassed=true;
-        String studentName="山田太郎";
-    }
-}
