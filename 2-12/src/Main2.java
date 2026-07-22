24fb26727ed2ad9670f764b9b9c329a9913d4ab3
YUAN.CHEN <yyh2205102568@gmail.com>
2026-07-23 04:00:02 +0900

    2-12_繰り返し（演習問題） (YUAN CHEN)

---
+++ 2-12/src/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args){
+        for(int i=1; i<=5;i++){
+            for(int j=1;j<=9;j++){
+                System.out.println(i+"x"+j+"="+(i*j));
+            }
+            System.out.println();
+        }
+    }
+}
