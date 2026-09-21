9bb19cf72ed5ee3438ef77b0f42fbaca1fedc756
YUAN.CHEN <yyh2205102568@gmail.com>
2026-09-22 04:30:24 +0900

    4-18_単体テストとアサーション（演習問題） （YUAN CHEN)

---
+++ 4-18.test/app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Calculator {
+
+    // 足し算
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // 引き算
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
